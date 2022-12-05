3c864d0c35c090e1fa752a56837cc1f2fab5cc30
dreamboxtech <bbbolaleye@gmail.com>
2022-12-05 01:00:08 +0100

    Date widget included

--- home/forms.py
+++ home/forms.py
@@ -2,6 +2,13 @@ from django import forms
 from .models import Project
 from crispy_forms.layout import Layout, HTML, Row, Column
 from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+
+# Date modules
+
+from django.contrib.admin.widgets import AdminDateWidget
+from django.forms.fields import DateField
+
 
 class ProjectForm(forms.ModelForm):
 
@@ -16,6 +23,7 @@ class ProjectForm(forms.ModelForm):
 	
 	class Meta:
 		model = Project
+		# fields = '__all__'
 		fields = (
 				'title',
 				'keywords',
@@ -25,19 +33,35 @@ class ProjectForm(forms.ModelForm):
 				'date_ended',
 				'images',
 			)
+		widgets = {
+	        'date_started': forms.TextInput(attrs={'type': 'date'}),
+	        'date_ended': forms.TextInput(attrs={'type': 'date'})
+    }
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
 		self.helper = FormHelper()
 		self.helper.layout = Layout(
 			 Row(
-				Column('title', css_class='form-group col-md-6 mb-0'),
-                Column('keywords', css_class='form-group col-md-6 mb-0 mx-3'),
+				Column('title', css_class='form-group col-md-6 mb-0 ml-2'),
+                Column('keywords', css_class='form-group col-md-5 mb-0 ml-4'),
               
 			),
 			 Row(
 				Column(
 					'description',
-					css_class='col-md-8'
-				),
+					css_class='col-md-10'
+				)
+			),
+			Row(
+				Column('stage', css_class='col-md-4'),
+				Column('date_started', css_class='col-md-4 mx-2'),
+				Column( 'date_ended', css_class='col-md-4')
+			),
+			Row(
+				Column('images', css_class='col-md-4'),
+			),
+			Row(
+				Submit('submit', 'Submit', css_class='col-md-5 float-right')
 			)
-		)
+				
+)
